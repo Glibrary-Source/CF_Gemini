f8e9b0724b5a3035eacb9f924629534897123f63
Kim <kimgyudo93@gmail.com>
2024-05-24 17:37:34 +0900

    first commit

--- cloud_function_call_gemini.py
+++ cloud_function_call_gemini.py
@@ -2,12 +2,10 @@ import google.generativeai as genai
 from PIL import Image
 import base64
 import io
+import os
 
-genai.configure(api_key="AIzaSyDRyVRhe0_wdN1zqAI-RwOb9tb0mWZFfsE")
-model = genai.GenerativeModel(
-    'gemini-1.5-flash-latest',
-    
-    )
+genai.configure(api_key=os.environ.get("GEMINI_KEY"))
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 def call_gemini_service(request):
     
@@ -25,6 +23,9 @@ def call_gemini_service(request):
         
         elif type == "call_img_with_prompt_gemini" and 'img_base64' in request_json and 'prompt' in request_json:
             return call_img_with_prompt_gemini(request_json['img_base64'], request_json['prompt'])
+        
+        elif type == "call_couple_img_with_prompt" and 'img_base64_1' in request_json and 'img_base64_2' in request_json and 'prompt' in request_json:
+            return call_couple_img_with_prompt(request_json['img_base64_1'], request_json['img_base64_2'], request_json['prompt'])
         
         else:
             return "Error: check body"
@@ -55,3 +56,17 @@ def call_img_with_prompt_gemini(img_base64, prompt):
     response = model.generate_content([prompt, img])
 
     return response.text
+
+def call_couple_img_with_prompt(img_base64_1,img_base64_2, prompt):
+    
+    img_data1 = base64.b64decode(img_base64_1)
+    img_data2 = base64.b64decode(img_base64_2)
+
+    img1 = Image.open(io.BytesIO(img_data1))
+    img2 = Image.open(io.BytesIO(img_data2))
+
+    response = model.generate_content([prompt, img1, img2])
+
+    return response.text
+
+print(call_only_text_gemini("hellow"))
